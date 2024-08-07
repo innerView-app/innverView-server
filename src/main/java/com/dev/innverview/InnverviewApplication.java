69098253e2286159cdba5d38d22e86d4eaafd2d9
chani1209 <01077590592@hanmail.net>
2024-08-07 11:51:15 +0900

    add spring project files

---
+++ src/main/java/com/dev/innverview/InnverviewApplication.java
@@ -0,0 +1,13 @@
+package com.dev.innverview;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InnverviewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InnverviewApplication.class, args);
+	}
+
+}
