aa088fb4a72f2341b9549a82f0cfdc1edd203c6a
chani1209 <01077590592@hanmail.net>
2025-01-17 18:43:26 +0900

    :memo: Writing docs.

---
+++ src/main/java/com/dev/innverview/config/SwaggerConfig.java
@@ -0,0 +1,21 @@
+package com.dev.innverview.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+
+public class SwaggerConfig {
+
+    public OpenAPI openAPI() {
+        return new OpenAPI()
+                .components(new Components())
+                .info(apiInfo());
+    }
+
+    private Info apiInfo() {
+        return new Info()
+                .title("Interview API")
+                .description("Interview API 명세서")
+                .version("1.0.0");
+    }
+}
